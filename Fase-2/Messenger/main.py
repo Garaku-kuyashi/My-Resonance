fdc17f692c1c386c35c9cae86c270139a3285716
Garaku-kuyashi <moraxz112@gmail.com>
2026-06-23 23:01:18 +0800

    Fase-2 - day 4(nambah fitur crud karakter)

--- Fase-2/Messenger/main.py
+++ Fase-2/Messenger/main.py
@@ -67,6 +67,14 @@ def jawaban_dummy(karakter, pesan_user):
         else:
             return "Maaf sensei, aku belum tahu bagaimana membalas pesan ini."
 
+    if "halo" in pesan_user or "hai" in pesan_user:
+                return "Uh ummm halo juga"
+    elif "belajar" in pesan_user:
+        return "Ya, kita bisa belajar"
+    else:
+        return "Umm... aku belum tahu harus membalas apa untuk pesan ini"
+    
+
 def buat_folder_chat():
     if not os.path.exists(FOLDER_CHAT):
         os.makedirs(FOLDER_CHAT)
@@ -122,18 +130,180 @@ def potong_preview(teks, batas=50):
         return teks[:batas] + "....."
     return teks
 
+def buat_nama_file(nama):
+    nama = nama.lower()
+    nama = nama.replace("", "_")
+    return nama + ".json"
+
+def tambah_karakter():
+    print("\n--- TAMBAH KARAKTER ---")
+
+    nama = input("Nama karakter: ").strip()
+
+    if not nama:
+        print("Nama tidak boleh kosong...")
+        return
+
+    for karakter in karakter_list:
+        if karakter["nama"].lower() == nama.lower():
+            print("Nama karakter sudah ada...")
+            return
+
+    status = input("Status (Online/Offline): ").strip()
+
+    if status.lower() == "online":
+        status = "Online"
+    else:
+        status = "Offline"
+
+    pesan_awal = input("Pesan awal: ").strip()
+
+    if not pesan_awal:
+        pesan_awal = "Belum ada pesan..."
+
+    gaya = input("Gaya karakter: ").strip()
+
+    if not gaya:
+        gaya = "ramah"
+
+    karakter_baru = {
+        "nama" : nama,
+        "status" : status,
+        "pesan_terakhir" : pesan_awal,
+        "file_chat" : buat_nama_file(nama),
+        "gaya" : gaya
+    }
+
+    karakter_list.append(karakter_baru)
+    simpan_data_karakter()
+
+    print(f"Karakter {nama} berhasil ditambahkan")
+
+def edit_karakter(nomor):
+    if nomor < 1 or nomor > len(karakter_list):
+        print("Nomor karakter tidak tersedia di daftar...")
+        return
+    karakter = karakter_list[nomor - 1]
+
+    print("\n--- EDIT KARAKTER ---")
+    print(f"Nama sekarang                 : {karakter['nama']}")
+    print(f"Status sekarang               : {karakter['status']}")
+    print(f"Gaya sekarang                 : {karakter['gaya']}")
+    print(f"Pesan terakhir sekarang       : {karakter['pesan_terakhir']}")
+    print("\nKosongkan input jika tidak ingin mengubah bagian tersebut.")
+
+    nama_baru = input("Nama baru: ").strip()
+    status_baru = input("Status baru (Online/Offline): ").strip()
+    gaya_baru = input("Gaya baru: ").strip()
+    pesan_baru = input("Pesan baru: ").strip()
+
+    if nama_baru:
+        for data in karakter_list:
+            if data["nama"].lower() == nama_baru.lower() and data != karakter:
+                print("Nama tersebut sudah digunakan pada karakter lain.")
+                return
+        file_lama = karakter["file_chat"]
+        file_baru = buat_nama_file(nama_baru)
+        path_lama = os.path.join("chats", file_lama)
+        path_baru = os.path.join("chats", file_baru)
+
+        if os.path.exists(path_lama):
+            os.rename(path_lama, path_baru)
+
+        karakter["nama"] = nama_baru
+        karakter["file_chat"] = file_baru
+
+    if status_baru:
+        if status_baru.lower() == "online":
+            karakter["status"] = "Online"
+        elif status_baru.lower() == "offline":
+            karakter["status"] = "Offline"
+        else:
+            print("Status tidak valid. Status lama tetap dipakai...")
+
+    if gaya_baru:
+        karakter["gaya"] = gaya_baru
+
+    if pesan_baru:
+        karakter["pesan_terakhir"] = pesan_baru
+
+    simpan_data_karakter()
+    print(f"Data karakter {karakter['nama']} berhasil diperbarui")
+
+def hapus_karakter(nomor):
+    if nomor < 1 or nomor > len(karakter_list):
+            print("Nomor karakter tidak tersedia di daftar...")
+            return
+    karakter = karakter_list[nomor - 1]
+
+    print("\n--- HAPUS KARAKTER ---")
+    print(f"Nama karakter   : {karakter['nama']}")
+    print(f"File chat       : {karakter['file_chat']}")
+
+    konfirmasi = input(f'Ketik "hapus" untuk menghapus {karakter["nama"]}: ').strip().lower()
+
+    if konfirmasi != "hapus":
+        print("Prose dibatalkan...")
+        return
+
+    path_chat = os.path.join("chats", karakter["file_chat"])
+
+    try:
+        if os.path.exists(path_chat):
+            os.remove(path_chat)
+            print("File chat berhasil dihapus...")
+        else:
+            print("File chat tidak ditemukan, tetapi data karakter tetap dihapus")
+
+        karakter_list.pop(nomor - 1)
+        simpan_data_karakter()
+        print(f"Karakter {karakter['nama']} berhasil dihapus...")
+    except Exception as e:
+        print("Gagal menghapus karakter:", e)
+
 load_data_karakter()
 
 while True:
     tampilkan_daftar_karakter()
-    pilihan = input("\nPilih karakter (1-3) atau ketik /exit untuk keluar: ").strip()
+    pilihan = input("\nPilih nomor karakter atau ketik /exit untuk keluar, /add untuk tambah karakter: ").strip()
 
     if pilihan.lower() == "/exit":
         print("Arigatou sensei, sampai jumpa lagi!")
         break
+    elif pilihan.lower() == "/add":
+        tambah_karakter()
+        continue
+    elif pilihan.lower().startswith("/edit"):
+        bagian_perintah = pilihan.split()
+
+        if len(bagian_perintah) != 2:
+            print("Format salah. Contoh penggunaan: /edit 4")
+            continue
+
+        if not bagian_perintah[1].isdigit():
+            print("Nomor karakter harus berupa angka...")
+            continue
+
+        nomor_edit = int(bagian_perintah[1])
+        edit_karakter(nomor_edit)
+        continue
+    elif pilihan.lower().startswith("/delete"):
+        bagian_perintah = pilihan.split()
+
+        if len(bagian_perintah) != 2:
+            print("Format salah. Contoh penggunaan: /edit 4")
+            continue
+        
+        if not bagian_perintah[1].isdigit():
+            print("Nomor karakter harus berupa angka...")
+            continue
+
+        nomor_hapus = int(bagian_perintah[1])
+        hapus_karakter(nomor_hapus)
+        continue
 
     if not pilihan.isdigit():
-        print("Pilihan tidak valid. Silakan masukkan nomor karakter atau /exit.")
+        print("Pilihan tidak valid. Silakan masukkan nomor karakter atau /exit atau /add.")
         continue
 
     pilihan = int(pilihan)
